09a69ca79b723cfb738c649931bc64866fa5f761
emaiannone <emaiannone96@gmail.com>
2020-05-30 17:24:21 +0200

    End session of the homework II
    I have to add some more tests for uncovered branches to check for other bugs

--- smosa/commons-cli/src/test/org/apache/commons/cli/OptionTest.java
+++ smosa/commons-cli/src/test/org/apache/commons/cli/OptionTest.java
@@ -8,6 +8,7 @@ public class OptionTest {
 
     /**
      * Test mainly for {@link Option#setArgName(String)}
+     * It has a bug: an empty arg ("") should make hasArgName() return false instead of true because it does not make sense (but it depends on its invariant)
      * @throws Throwable
      */
   @Test
@@ -15,11 +16,12 @@ public class OptionTest {
       Option option0 = new Option("", "");
       option0.setArgName("");
       boolean boolean0 = option0.hasArgName();
-      assertFalse(boolean0);  // an empty arg ("") should make setArgName() return false instead of true because it does not make sense (it depends on its invariant)
+      assertFalse(boolean0);  // FIXED
   }
 
     /**
      * Test mainly for {@link Option#getId()}
+     * It has a bug: id should be 'a', not 'r'
      * @throws Throwable
      */
   @Test
@@ -29,12 +31,12 @@ public class OptionTest {
       assertEquals("It should have default arg name", "arg", option0.getArgName());
       assertFalse("It should not have a long name", option0.hasLongOpt());
       assertFalse("It should not accept any arg value", option0.hasArgs());
-      assertEquals("ID should be letter 'a'", 'a', int0);  // Letter 'r' --> 'a'
+      assertEquals("ID should be letter 'a'", 'a', int0);  // FIXED
   }
 
-    //////// The following two tests for getId() show the fact that charAt(1) should be replaced with charAt(0) ///////
     /**
-     * Test for {@link Option#getId()}
+     * Additional test for {@link Option#getId()}
+     * It shows the fact that charAt(1) should be replaced with charAt(0)
      */
     @Test
     public void getIdTestSingleCharacter() {
@@ -44,7 +46,8 @@ public class OptionTest {
     }
 
     /**
-     * Test for {@link Option#getId()}
+     * Additional test for {@link Option#getId()}
+     * It shows the fact that charAt(1) should be replaced with charAt(0)
      */
     @Test
     public void getIdTestTwoCharacters() {
@@ -54,15 +57,17 @@ public class OptionTest {
     }
 
     /**
-     * Test that seems for {@link Option#getKey()}, but it does not check anything on it.
+     * Test that seems for {@link Option#getKey()}
+     * It does not check anything on it: an assertion was added
      * @throws Throwable
      */
   @Test
   public void test03()  throws Throwable  {
       Option option0 = new Option((String) null, (String) null);
-      option0.getKey();
+      String key = option0.getKey();
       assertEquals("It should have default arg name", "arg", option0.getArgName());
       assertFalse("It should not accept any arg value", option0.hasArgs());
+      assertNull("It should be null", key);
   }
 
     /**
@@ -79,9 +84,9 @@ public class OptionTest {
       assertEquals("It should accept exactly one arg value", 1, option0.getArgs());
   }
 
-    //////// The following test for hasArgs() show the fact that 0 is a valid numberOfArgs, when it should not ///////
     /**
-     * Test for {@link Option#hasArgs()}
+     * Additional test for {@link Option#hasArgs()}
+     * It shows the fact that 0 is a valid numberOfArgs, when it should not
      */
     @Test
     public void hasArgsNumber0() {
@@ -92,6 +97,7 @@ public class OptionTest {
 
     /**
      * Test mainly for {@link Option#equals(Object)}
+     * It has a bug: if option0 has longOpt==null and option1 has longOpt=="", it passes when it should not
      * @throws Throwable
      */
   @Test
@@ -99,14 +105,15 @@ public class OptionTest {
       Option option0 = new Option("", "");
       Option option1 = new Option("", "", false, "");
       boolean boolean0 = option0.equals(option1);
-      assertEquals("arg", option1.getArgName());
-      assertFalse(option0.hasLongOpt());
-      assertFalse(option1.hasArgs());
-      assertTrue(boolean0);
+      assertEquals("2nd option should have default arg name", "arg", option1.getArgName());
+      assertFalse("1st option should not have a long name", option0.hasLongOpt());
+      assertFalse("2nd option should not accept any arg value", option1.hasArgs());
+      assertFalse("The two options should be not equals", boolean0);  // FIXED
   }
 
     /**
      * Test mainly for {@link Option#equals(Object)}
+     * It has a bug: if option0 has longOpt=="" and option1 has longOpt==null, it passes when it should not
      * @throws Throwable
      */
   @Test
@@ -114,11 +121,11 @@ public class OptionTest {
       Option option0 = new Option((String) null, "", false, "");
       Option option1 = new Option((String) null, "");
       boolean boolean0 = option0.equals(option1);
-      assertTrue(option0.hasLongOpt());
-      assertTrue(boolean0);
-      assertEquals("arg", option1.getArgName());
-      assertFalse(option1.hasLongOpt());
-      assertFalse(option1.hasArgs());
+      assertTrue("1st option should have a long name", option0.hasLongOpt());
+      assertFalse("The two options should be not equals", boolean0);  // FIXED
+      assertEquals("2nd option should have default arg name", "arg", option1.getArgName());
+      assertFalse("2nd option should not have a long name", option1.hasLongOpt());
+      assertFalse("2nd option should not accept any arg value", option1.hasArgs());
   }
 
     /**
@@ -130,13 +137,24 @@ public class OptionTest {
       Option option0 = new Option("", "Pz");
       Option option1 = new Option("Pz", "");
       boolean boolean0 = option0.equals(option1);
-      assertFalse(boolean0);
-      assertEquals((-1), option1.getArgs());
-      assertFalse(option1.hasLongOpt());
-      assertEquals("", option1.getDescription());
-      assertEquals("Pz", option1.getOpt());
-      assertEquals("arg", option1.getArgName());
+      assertFalse("The two options should be not equals", boolean0);
+      assertEquals("2nd option should accept no arg values", (-1), option1.getArgs());
+      assertFalse("2nd option should not have a long name", option1.hasLongOpt());
+      assertEquals("2nd option shot have empty description", "", option1.getDescription());
+      assertEquals("1st option should be \"Pz\"", "Pz", option1.getOpt());
+      assertEquals("2nd option should have default arg name", "arg", option1.getArgName());
   }
+
+    /**
+     * Additional test for {@link Option#equals(Object)}
+     * It shows the fact that equals() does not correctly check longOpt (line 613 should return false)
+     */
+    @Test
+    public void equalsDifferentLongOpt() {
+        Option option0 = new Option("v", "verbose", false, "");
+        Option option1 = new Option("v", "not_verbose", false, "");
+        assertNotEquals("The two options should be not equals", option0, option1);
+    }
 
     /**
      * Test mainly for {@link Option#hasArg()}
@@ -146,9 +164,9 @@ public class OptionTest {
   public void test08()  throws Throwable  {
       Option option0 = new Option((String) null, (String) null);
       boolean boolean0 = option0.hasArg();
-      assertFalse(boolean0);
-      assertEquals((-1), option0.getArgs());
-      assertEquals("arg", option0.getArgName());
+      assertFalse("It should not accept any arg value", boolean0);
+      assertEquals("It should accept no arg values", (-1), option0.getArgs());
+      assertEquals("It should have default arg name", "arg", option0.getArgName());
   }
 
     /**
@@ -160,24 +178,32 @@ public class OptionTest {
       Option option0 = new Option("", "", true, "");
       Option option1 = (Option)option0.clone();
       boolean boolean0 = option0.equals(option1);
-      assertEquals("arg", option1.getArgName());
-      assertTrue(boolean0);
-      assertTrue(option1.hasArg());
-      assertNotSame(option1, option0);
+      assertEquals("2nd option should have default arg name", "arg", option1.getArgName());
+      assertTrue("The two clones should be equal", boolean0);
+      assertTrue("2nd option should accept at least one arg value", option1.hasArgs());
+      assertNotSame("The two clones should be not the very same object", option1, option0);
   }
 
     /**
-     * Test mainly for {@link Option#getOpt()}
+     * Test that seems for {@link Option#getOpt()}
+     * It does not check anything on it: an assertion was added
+     *
      * @throws Throwable
      */
   @Test
   public void test10()  throws Throwable  {
       Option option0 = new Option("NO_ARGS_ALLOWED", "NO_ARGS_ALLOWED");
-      option0.getOpt();
-      assertEquals("arg", option0.getArgName());
-      assertFalse(option0.hasLongOpt());
-      assertFalse(option0.hasArgs());
+      String opt = option0.getOpt();
+      assertEquals("It should have default arg name", "arg", option0.getArgName());
+      assertFalse("It should not have a long name", option0.hasLongOpt());
+      assertFalse("It should not accept any arg value", option0.hasArgs());
+      assertEquals("It should be the one passed on costruction", "NO_ARGS_ALLOWED", opt);
   }
+
+  // TODO Add tests for uncovered methods/branches
+    // addValueForProcessing
+    // getValue
+    // getValues
 
 
 
